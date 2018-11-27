5edebfc96a824fdca20eb3185005e23f80c6ab43
Eugen <eugen@baeldung.com>
2018-11-27 03:20:52 -0800

    initial Boot 2 commit

---
+++ m11-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {// @formatter:off
+        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    } // @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+        http
+        .authorizeRequests()
+                .antMatchers("/badUser*",
+                        "/js/**").permitAll()
+                .anyRequest().authenticated()
+
+        .and()
+        .formLogin().
+            loginPage("/login").permitAll().
+            loginProcessingUrl("/doLogin")
+
+        .and()
+        .logout().permitAll().logoutUrl("/logout")
+
+        .and()
+        .csrf().disable()
+        ;
+    } // @formatter:on
+
+}
