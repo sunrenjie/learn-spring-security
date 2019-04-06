d8fea508db7785fe897bf6a3b12ce39d0fb7ee82
Loredana <lore.crusoveanu@gmail.com>
2019-04-06 16:41:30 +0300

    update deprecated classes, clean pom

--- m4-lesson1/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
+++ m4-lesson1/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
@@ -1,15 +1,17 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 //@EnableWebMvc
-public class LssWebMvcConfiguration extends WebMvcConfigurerAdapter {
+@Configuration
+public class LssWebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
--- m4-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m4-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan({ "org.baeldung.lss.security" })
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public LssSecurityConfig() {
         super();
@@ -25,7 +28,7 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
