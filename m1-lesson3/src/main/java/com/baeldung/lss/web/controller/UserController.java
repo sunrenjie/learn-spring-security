14ac220466a93a9557b0aab3e0b1b587ca5b7562
Renjie Sun <sunrenjie6@gmail.com>
2020-01-09 14:24:51 +0800

    [m1-lession3] Fix URL mapping to more reasonable.
    Motivation:
    Access to /favico.ico is translated to loading of a user with this name.
    
    Changes:
    The URL mapping is formulated as thus:
    * A context path is defined for the module.
    * /users/ for operations related to all users
    * /user/{user-id}/ for operations related to an individual user.
    
    Results:
    * Now we have more natural URL mappings.

--- m1-lesson3/src/main/java/com/baeldung/lss/web/controller/UserController.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/web/controller/UserController.java
@@ -16,7 +16,6 @@ import com.baeldung.lss.persistence.UserRepository;
 import com.baeldung.lss.web.model.User;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -28,30 +27,35 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping
+    @RequestMapping("/users")
     public ModelAndView list() {
         Iterable<User> users = this.userRepository.findAll();
         return new ModelAndView("users/list", "users", users);
     }
 
-    @RequestMapping("{id}")
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/users";
+    }
+
+    @RequestMapping("/user/{id}")
     public ModelAndView view(@PathVariable("id") User user) {
         return new ModelAndView("users/view", "user", user);
     }
 
-    @RequestMapping(params = "form", method = RequestMethod.GET)
+    @RequestMapping(path = "/users/create", method = RequestMethod.GET)
     public String createForm(@ModelAttribute User user) {
         return "users/form";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(path = "/users/create", method = RequestMethod.POST)
     public ModelAndView create(@Valid User user, BindingResult result, RedirectAttributes redirect) {
         if (result.hasErrors()) {
             return new ModelAndView("users/form", "formErrors", result.getAllErrors());
         }
         user = this.userRepository.save(user);
         redirect.addFlashAttribute("globalMessage", "Successfully created a new user");
-        return new ModelAndView("redirect:/{user.id}", "user.id", user.getId());
+        return new ModelAndView("redirect:/user/{user.id}", "user.id", user.getId());
     }
 
     @RequestMapping("foo")
@@ -59,13 +63,13 @@ public class UserController {
         throw new RuntimeException("Expected exception in controller");
     }
 
-    @RequestMapping(value = "delete/{id}")
+    @RequestMapping(value = "/user/{id}/delete")
     public ModelAndView delete(@PathVariable("id") Long id) {
         this.userRepository.deleteUser(id);
-        return new ModelAndView("redirect:/");
+        return new ModelAndView("redirect:/users");
     }
 
-    @RequestMapping(value = "modify/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/{id}/modify", method = RequestMethod.GET)
     public ModelAndView modifyForm(@PathVariable("id") User user) {
         return new ModelAndView("users/form", "user", user);
     }
